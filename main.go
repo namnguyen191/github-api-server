17911d72b8199e66cae2c23da899724d27a071fc
Nam Nguyen <hoangnamnguyen191@gmail.com>
2022-09-27 12:19:39 -0400

    feat: update legacy code

--- main.go
+++ main.go
@@ -2,52 +2,18 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/joho/godotenv"
+	"github.com/namnguyen191/github-api-server/api"
 )
 
 const defaultMainPort = "8000"
 
 type appConfig struct {
 	mainPort string
-}
-
-func getNamRepos(w http.ResponseWriter, _ *http.Request) {
-	const NamReposUrl = "https://api.github.com/users/namnguyen191/repos"
-	client := &http.Client{
-		Timeout: 30 * time.Second,
-	}
-	req, err := http.NewRequest("GET", NamReposUrl, nil)
-	if err != nil {
-		fmt.Println("Error constructing request: ", err)
-		w.Write([]byte("Something went wrong"))
-		return
-	}
-
-	token := os.Getenv("GITHUB_API_TOKEN")
-	req.Header.Set("authorization", fmt.Sprintf("token %s", token))
-
-	resp, err := client.Do(req)
-	if err != nil || resp.StatusCode != http.StatusOK {
-		fmt.Println("Error fetching repos: ", err)
-		w.Write([]byte("Something went wrong"))
-		return
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error parsing repos body: ", err)
-		w.Write([]byte("Something went wrong"))
-	}
-
-	w.Header().Add("content-type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(body))
 }
 
 func main() {
@@ -58,7 +24,7 @@ func main() {
 
 	app := &appConfig{}
 
-	http.HandleFunc("/", getNamRepos)
+	http.HandleFunc("/", api.Handler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
